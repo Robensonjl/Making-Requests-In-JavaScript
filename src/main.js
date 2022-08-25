aa989c1dc4fc6a1ec7c0818077187ed4d7c0ff18
Robenson <robenson76@gmail.com>
2022-08-24 19:40:32 -0500

    Final completed project upload

--- src/main.js
+++ src/main.js
@@ -1,9 +1,28 @@
 const axios = require("../utils/axios");
 const BASE_URL = "http://localhost:5000";
+const url = "http://localhost:5000/constellations";
 
-function getAllNames() {}
+function getAllNames() {
+  axios
+    .get(url)
+    .then((response) => {
+      const named = response.data.map((constellation) => constellation.name);
+      console.log(named);
+    })
+    .catch((error) => console.log(error.message));
+}
 
-function getConstellationsByQuadrant(quadrant) {}
+function getConstellationsByQuadrant(quadrant) {
+  axios
+    .get(url)
+    .then((response) => {
+      const filtered = response.data.filter((constellation) => {
+        return constellation.quadrant == quadrant;
+      });
+      console.log(filtered);
+    })
+    .catch((error) => console.log(error.message));
+}
 
 module.exports = {
   getAllNames,
